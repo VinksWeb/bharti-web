09ff6eded211189224f383b616478d7eaf8d6f2b
VinksWeb <vinayak.vicky.sutar3@gmail.com>
2017-08-17 20:12:25 +0530

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,8 +85,8 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documents</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://laravel-news.com">News </a>
+                    <a href="https://forge.laravel.com">Forget</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
